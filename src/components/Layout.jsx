38ad3ea7be5188d71495bc1e489d6e621b580ced
fire9900 <37242708+fire9900@users.noreply.github.com>
2025-12-01 12:12:38 +0400

    Add footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
-import { NavLink, Outlet } from "react-router-dom"
+import {NavLink, Outlet} from "react-router-dom"
+import { HashLink } from 'react-router-hash-link'
 import "../assets/styles/layout.scss"
 
 const Layout = () => {
@@ -22,7 +23,29 @@ const Layout = () => {
             <main>
                 <Outlet/>
             </main>
-            <footer>2025</footer>
+            <footer className="footer">
+                <div className="footer-column">
+                    <p className="footer-column__name footer-column__name--title">OpOphtolmology</p>
+                </div>
+                <div className="footer-column">
+                    <p className="footer-column__name">Тарифы</p>
+                    <HashLink to="/#pricing-section" className="footer-column__link">Эконом</HashLink>
+                    <HashLink to="/#pricing-section" className="footer-column__link">Стандарт</HashLink>
+                    <HashLink to="/#pricing-section" className="footer-column__link">Профессионал</HashLink>
+                </div>
+                <div className="footer-column">
+                    <p className="footer-column__name">Особенности</p>
+                    <HashLink to="/#dignities-section" className="footer-column__link">Основные</HashLink>
+                    <HashLink to="/#dignities-section" className="footer-column__link">Продвинутые</HashLink>
+                    <HashLink to="/#dignities-section" className="footer-column__link">Интеграции</HashLink>
+                </div>
+                <div className="footer-column">
+                    <p className="footer-column__name">Связь</p>
+                    <HashLink to="#" className="footer-column__link">Контакты</HashLink>
+                    <HashLink to="#" className="footer-column__link">Поддержка</HashLink>
+                    <HashLink to="#" className="footer-column__link">Пользовательское соглашение</HashLink>
+                </div>
+            </footer>
         </>
     )
 }
